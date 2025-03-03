950f50cd33d81f34845b6260cce626d8d43ebd2a
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-03-03 17:16:03 +0800

    layout and intro added

--- website-portfolio/src/routes/+layout.svelte
+++ website-portfolio/src/routes/+layout.svelte
@@ -15,7 +15,16 @@
 
 <Toaster />
 <ModeWatcher />
-<div class="p-2 w-full justify-end flex">
+<div class="p-2 mb-20 w-full justify-end flex">
 	<Switch bind:checked={toggled}/>
 </div>
-<slot />
+<div class="flex w-full">
+	<div class="w-1/2 flex flex-col items-center pt-10">
+		<div class="flex flex-col text-right gap-3">
+			<a href="/">about</a>
+			<a href="/notes">notes</a>
+			<a href="/projects">projects</a>
+		</div>
+	</div>
+	<slot />
+</div>	
--- website-portfolio/src/routes/+page.svelte
+++ website-portfolio/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<div class="w-full flex flex-col justify-center pl-4 gap-10">
+    <div class="w-3/4">
+        <h1 class="text-xl mb-4">Alexander Seah</h1>
+        <p>I like making tools to solve my personal pain points. 
+            <br> Big fan of working in 'simple' languages like Go, Svelte, and Swift.
+        </p>
+        <br> 
+        <p>
+            When I'm not coding, I enjoy playing the trumpet, basketball, and video games like minecraft and wildrift.
+        </p>
+        <br>
+        <p>The is a page for sharing some of my own projects and notes I thought might be helpful and interesting.</p>
+    </div>
+</div>
