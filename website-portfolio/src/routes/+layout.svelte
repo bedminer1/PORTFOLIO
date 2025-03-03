9834f77ac94e908448b7c0770272bcf000db0548
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-03-03 12:55:33 +0800

    init dark mode switch

--- website-portfolio/src/routes/+layout.svelte
+++ website-portfolio/src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+	import '../app.css'
+	import { ModeWatcher, toggleMode } from 'mode-watcher'
+	import { Toaster } from "$lib/components/ui/sonner";
+  	import { Switch } from '$lib/components/ui/switch';
+
+	let toggled = false
+
+	$: {
+		if (toggled) {}
+	 	toggleMode() 
+	}
+	toggleMode()
 </script>
 
-{@render children()}
+<Toaster />
+<ModeWatcher />
+<div class="p-2 w-full justify-end flex">
+	<Switch bind:checked={toggled}/>
+</div>
+<slot />
