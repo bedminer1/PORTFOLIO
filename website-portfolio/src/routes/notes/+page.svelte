2997beff8e7921cdbc890869136023affac1fd31
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-03-04 15:38:11 +0800

    added functionality for notes

--- website-portfolio/src/routes/notes/+page.svelte
+++ website-portfolio/src/routes/notes/+page.svelte
@@ -1,22 +1,55 @@
 <script lang="ts">
     import { mode } from "mode-watcher"
+
+    interface Note {
+        title: String
+        date: String
+        text: String
+    }
+
+    let noteReadingMode = false
+    let selectedNote: Note | undefined = undefined
+    let notes: Note[] = [
+        {
+            title: "test title",
+            date: "4 MAR 2024",
+            text: "test body"
+        },
+        {
+            title: "test title 2",
+            date: "4 MAR 2024",
+            text: "test body 2"
+        },
+    ]
+
+    function handleSelectNote(note: Note) {
+        noteReadingMode = true
+        selectedNote = note
+    }
+
+    function handleUnselectNote() {
+        noteReadingMode = false
+        selectedNote = undefined
+    }
 </script>
 
 <div class="w-full flex flex-col justify-center pl-4 gap-10">
     <div class="w-3/4">
-        <h1 class="text-xl mb-10">Notes</h1>
-        <div class={$mode === "light" ? "text-gray-600" : "text-gray-300"}>
-            <p>
-                I'm a developer based in Singapore. I like making tools to solve my own pain points, mostly using 'simple' (but productive) languages like Go and Svelte. 
-            </p>
-            <br>
-            <p>
-                Some other hobbies include jamming out jazz solos on the trumpet, playing ADC on Wildrift with my support duo, and working on post moves to bust out at Sunday morning basketball pick-up. 
-            </p>
-            <br>
-            <p>
-                If any of my <a class="underline" href="/projects">projects</a>, <a class="underline" href="/notes">notes</a>, or hobbies seem interesting, I'm always down to talk — feel free to reach out on <a target="_blank" class="underline" href="https://x.com/bedminer1">Twitter</a> or <a target="_blank" class="underline" href="https://www.linkedin.com/in/alexander-seah-4a23622b5/">LinkedIn</a>!
-            </p>
+        <div class={$mode === "light" ? "text-gray-600 hover:text-gray-800" : "text-gray-300 hover:text-white"}>
+            {#if noteReadingMode}
+                <div class="pt-10">
+                    <h1 class="text-xl mb-4 {$mode === "light" ? "text-gray-800" : "text-gray-200"}">{selectedNote!.title}</h1>
+                    <p>{selectedNote!.text}</p>
+                    <button onclick={handleUnselectNote}>&larr;</button>
+                </div>
+            {:else}
+                {#each notes as note}
+                <button class="flex w-full justify-between" onclick={() => handleSelectNote(note)}>
+                    <p>{note.title}</p>
+                    <p>{note.date}</p>
+                </button>
+                {/each}
+            {/if}
         </div>
     </div>
 </div>
