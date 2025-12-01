9a0d9eb6354574fd6e0493125d551b4f436423d3
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-12-01 22:57:11 +0800

    updated about page

--- website-portfolio/src/routes/+page.svelte
+++ website-portfolio/src/routes/+page.svelte
@@ -11,16 +11,23 @@
         </div>
         <div class={$mode === "light" ? "text-gray-600" : "text-gray-300"}>
             <p>
-                I'm a developer based in Singapore. I like making tools to solve my own pain points, mostly using 'simple' (but productive) languages like Go and Svelte. 
+                Hi, I'm a CS student from NUS. I like problems that require clever math to solve, like optimizing game strategies (maximizing expected value, minimizing risks). I like games like bridge so much I built a bot you can play against <a class="underline dark:text-red-400 text-red-700" target="_blank" href="https://bridge-psi-ten.vercel.app/">here</a>, try it and let me know if you liked it :)
             </p>
             <br>
             <p>
-                Some other hobbies include jamming out jazz <a class="underline" target="_blank" href="https://www.youtube.com/watch?v=gJ9WBYmSO4w&list=PLxe0KcilTZ5BJYFjTUjZTkvazRwZSAAb7&pp=gAQB">trumpet and guitar solos</a> and working on Giannis-inspired moves for pick-up basketball runs. 
+                Nowadays, I'm mostly focused on robotics in my <a class="underline dark:text-red-400 text-red-700" target="_blank" href="https://www.nusrobomaster.com/">school's club</a> for building war robots. I'm also building a traffic management during marathons-planner/robot as an idea I successfully pitched to my school's entrepreneur fund. 
             </p>
             <br>
             <p>
-                If my <a class="underline" href="/projects">projects</a>, <a class="underline" href="/notes">notes</a>, or hobbies seem interesting, I'm always down to talk — feel free to reach out on <a target="_blank" class="underline" href="https://x.com/bedminer1">Twitter</a> or <a target="_blank" class="underline" href="https://www.instagram.com/dkffjlrtm/">Instagram</a>!
+                Stuff I use
             </p>
+            <ol class="ml-4">
+                <li>- <a class="underline dark:text-red-400 text-red-700" target="_blank" href="https://strava.app.link/QaUm8XGkKYb">strava</a></li>
+                <li>- <a class="underline dark:text-red-400 text-red-700" target="_blank" href="https://strava.app.link/QaUm8XGkKYb">spotify</a></li>
+                <li>- <a class="underline dark:text-red-400 text-red-700" target="_blank" href="https://github.com/bedminer1">github</a></li>
+            </ol>
         </div>
     </div>
 </div>
+
+<p class="text-red-700 text-red-200"></p>
